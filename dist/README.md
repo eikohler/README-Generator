a6e3073688676daec0b81c7c537788af26d60297
Eric Kohler <erickohler1600@gmail.com>
2022-05-07 20:03:00 -0400

    prompts user for main sections, fills readme

--- dist/README.md
+++ dist/README.md
@@ -1,38 +1,25 @@
-# <Your-Project-Title>
+# README Generator
 
   ## Description
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque accumsan sodales lacus, sed molestie augue vehicula et. Pellentesque in nunc nec quam vestibulum pulvinar.
   
-  Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-  
-  - What was your motivation?
-  - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-  - What problem does it solve?
-  - What did you learn?
-  
-  ## Table of Contents (Optional)
-  
-  If your README is long, add a table of contents to make it easy for users to find what they need.
-  
+  ## Table of Contents
+    
   - [Installation](#installation)
   - [Usage](#usage)
-  - [Credits](#credits)
   - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
   
   ## Installation
-  
-  What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque accumsan sodales lacus, sed molestie augue vehicula et. Pellentesque in nunc nec quam vestibulum pulvinar.
   
   ## Usage
   
-  Provide instructions and examples for use. Include screenshots as needed.  
-  
-  ## Credits
-  
-  List your collaborators, if any, with links to their GitHub profiles.
-  
-  If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-  
-  If you followed tutorials, include links to those here as well.
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque accumsan sodales lacus, sed molestie augue vehicula et. Pellentesque in nunc nec quam vestibulum pulvinar.
   
   ## License
   
@@ -46,16 +33,18 @@
   
   ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
   
-  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.    
   
-  ## Features
+  ## Contributing
   
-  If your project has a lot of features, list them here.
-  
-  ## How to Contribute
-  
-  If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque accumsan sodales lacus, sed molestie augue vehicula et. Pellentesque in nunc nec quam vestibulum pulvinar.
   
   ## Tests
   
-  Go the extra mile and write tests for your application. Then provide examples on how to run them here.
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque accumsan sodales lacus, sed molestie augue vehicula et. Pellentesque in nunc nec quam vestibulum pulvinar.
+  
+  ## Questions
+  
+  - Github Username: eikohler
+  - Link to Github: https://github.com/eikohler
+  - If you have any questions, please send me an email at erickohler1600@gmail.com
--- index.js
+++ index.js
@@ -4,6 +4,17 @@ const generateMarkdown = require('./src/generateMarkdown.js');
 
 const fs = require('fs');
 
+const mockData = {
+    title: 'README Generator',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque accumsan sodales lacus, sed molestie augue vehicula et. Pellentesque in nunc nec quam vestibulum pulvinar.',
+    installation: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque accumsan sodales lacus, sed molestie augue vehicula et. Pellentesque in nunc nec quam vestibulum pulvinar.',
+    usage: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque accumsan sodales lacus, sed molestie augue vehicula et. Pellentesque in nunc nec quam vestibulum pulvinar.',
+    contributing: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque accumsan sodales lacus, sed molestie augue vehicula et. Pellentesque in nunc nec quam vestibulum pulvinar.',
+    tests: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque accumsan sodales lacus, sed molestie augue vehicula et. Pellentesque in nunc nec quam vestibulum pulvinar.',
+    github: 'eikohler',
+    email: 'erickohler1600@gmail.com',
+};
+
 const promptUser = () => {
     return inquirer.prompt([
       {
@@ -31,43 +42,101 @@ const promptUser = () => {
             return false;
           }
         }
-      },
+      },      
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps required to install your project?',
+        validate: projectInst => {
+          if (projectInst) {
+            return true;
+          } else {
+            console.log('Please enter your installation steps!');
+            return false;
+          }
+        }
+      },      
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What are the steps required to use your project?',
+        validate: projectUse => {
+          if (projectUse) {
+            return true;
+          } else {
+            console.log('Please enter your usage steps!');
+            return false;
+          }
+        }
+      },      
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'What are the contribution guidelines for your project?',
+        validate: projectCont => {
+          if (projectCont) {
+            return true;
+          } else {
+            console.log('Please enter the contribution guidelines!');
+            return false;
+          }
+        }
+      },      
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'What are some tests for your project?',
+        validate: projectCont => {
+          if (projectCont) {
+            return true;
+          } else {
+            console.log('Please enter a test for your project!');
+            return false;
+          }
+        }
+      },      
       {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub Username',
-        validate: gitName => {
-          if (gitName) {
+        message: 'What is your Github username?',
+        validate: projectGit => {
+          if (projectGit) {
             return true;
           } else {
             console.log('Please enter your Github Username!');
             return false;
           }
         }
-      },
-      {
-        type: 'confirm',
-        name: 'confirmAbout',
-        message: 'Would you like to enter some information about yourself for an "About" section?',
-        default: true
-      },
+      },      
       {
         type: 'input',
-        name: 'about',
-        message: 'Provide some information about yourself:',
-        when: ({ confirmAbout }) => {
-          if (confirmAbout) {
+        name: 'email',
+        message: 'What is your email?',
+        validate: projectEmail => {
+          if (projectEmail) {
             return true;
           } else {
+            console.log('Please enter your email!');
             return false;
           }
         }
-      }
+      },      
     ]);
   };
 
-fs.writeFile('./dist/README.md', generateMarkdown(), err => {
-    if (err) throw new Error(err);
-    console.log('Readme created!');
+promptUser().then(data => {
+    console.log(data);
+    fs.writeFile('./dist/README.md', generateMarkdown(data), err => {
+        if (err) throw new Error(err);
+        console.log('Readme created! Check in dist folder.');
+    });
 });
+
+// fs.writeFile('./dist/README.md', generateMarkdown(mockData), err => {
+//     if (err) throw new Error(err);
+//     console.log('Readme created! Check in dist folder.');
+// });
+  
+
+
 
